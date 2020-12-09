bc6052356d56db3c1e9c86ee35c4f21c55918866
Sebastian T F <sebastian.tf97@gmail.com>
2020-12-09 21:19:17 +0530

    define Employee model

---
+++ src/main/java/com/example/employeemgmt/model/Employee.java
@@ -0,0 +1,76 @@
+package com.example.employeemgmt.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int empid;
+    private String name;
+    private int age;
+    private String gender;
+    private String designation;
+    private String department;
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empid=" + empid +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", designation='" + designation + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
