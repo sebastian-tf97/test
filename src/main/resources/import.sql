2984f32904274e0e7a14a905cd531ed22925cf81
Sebastian T F <sebastian.tf97@gmail.com>
2020-12-10 13:51:07 +0530

    add Employee B with duplicate name

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,4 @@ INSERT INTO employees (empid, name, age, gender, designation, department) VALUES
 INSERT INTO employees (empid, name, age, gender, designation, department) VALUES (1002, "Employee C", 23, "M", "Recruiter", "HR");
 INSERT INTO employees (empid, name, age, gender, designation, department) VALUES (1003, "Employee D", 23, "F", "Systems Engineer", "IT");
 INSERT INTO employees (empid, name, age, gender, designation, department) VALUES (1004, "Employee E", 23, "M", "Database Administrator", "IT");
+INSERT INTO employees (empid, name, age, gender, designation, department) VALUES (1005, "Employee B", 23, "M", "Database Administrator", "IT");
