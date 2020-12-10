7751fae81fa0c9d4a797f8e0401360d3f834455d
Sebastian T F <sebastian.tf97@gmail.com>
2020-12-10 13:49:42 +0530

    implement EmployeeService

--- src/main/java/com/example/employeemgmt/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeemgmt/repository/EmployeeRepository.java
@@ -4,7 +4,9 @@ import com.example.employeemgmt.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-
+    List<Employee> findByName(String name);
 }
---
+++ src/main/java/com/example/employeemgmt/service/EmployeeService.java
@@ -0,0 +1,32 @@
+package com.example.employeemgmt.service;
+
+import com.example.employeemgmt.model.Employee;
+import com.example.employeemgmt.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepo;
+
+    public List<Employee> getAllEmployees() {
+        return (List<Employee>) employeeRepo.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(int empid) {
+        return employeeRepo.findById(empid);
+    }
+
+    public List<Employee> getEmployeeByName(String name) {
+        return employeeRepo.findByName(name);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+}
