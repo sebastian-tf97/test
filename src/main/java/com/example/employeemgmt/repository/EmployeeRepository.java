8a1c03a65b346b719716b8548ae412a02be4a5cd
Sebastian T F <sebastian.tf97@gmail.com>
2020-12-10 13:48:41 +0530

    implement EmployeeRepository

---
+++ src/main/java/com/example/employeemgmt/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.employeemgmt.repository;
+
+import com.example.employeemgmt.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
