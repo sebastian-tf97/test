c4a968be851110ed3554fca2136b1bf22f1ee3cb
Sebastian T F <sebastian.tf97@gmail.com>
2020-12-11 16:24:23 +0530

    implement EmployeeRepositoryIntegrationTest

---
+++ src/test/java/com/example/employeemgmt/repository/EmployeeRepositoryIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.example.employeemgmt.repository;
+
+import com.example.employeemgmt.model.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class EmployeeRepositoryIntegrationTest {
+
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    void findByName() {
+        Employee employeeA = new Employee();
+        employeeA.setName("Employee A");
+        employeeA.setAge(23);
+        employeeA.setGender("M");
+        employeeA.setDesignation("Systems Engineer");
+        employeeA.setDepartment("IT");
+
+        testEntityManager.persist(employeeA);
+
+        List<Employee> result = employeeRepository.findByName("Employee A");
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+        assertEquals("Employee A", result.get(0).getName());
+    }
+}
