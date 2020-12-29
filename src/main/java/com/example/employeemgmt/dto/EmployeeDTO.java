162c5e6b5aa33d41116de906c569236595748e69
Sebastian T F <sebastian.tf97@gmail.com>
2020-12-30 00:35:04 +0530

    use Lombok

--- src/main/java/com/example/employeemgmt/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeemgmt/dto/EmployeeDTO.java
@@ -1,40 +1,12 @@
 package com.example.employeemgmt.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class EmployeeDTO {
     private int empid;
     private String name;
     private String designation;
     private String department;
-
-    public int getEmpid() {
-        return empid;
-    }
-
-    public void setEmpid(int empid) {
-        this.empid = empid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 }
--- src/main/java/com/example/employeemgmt/model/Employee.java
+++ src/main/java/com/example/employeemgmt/model/Employee.java
@@ -1,10 +1,15 @@
 package com.example.employeemgmt.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "employees")
+@Getter @Setter @ToString
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,64 +21,4 @@ public class Employee {
     @NotEmpty
     private String designation;
     private String department;
-
-    public int getEmpid() {
-        return empid;
-    }
-
-    public void setEmpid(int empid) {
-        this.empid = empid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "empid=" + empid +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", gender='" + gender + '\'' +
-                ", designation='" + designation + '\'' +
-                ", department='" + department + '\'' +
-                '}';
-    }
 }
