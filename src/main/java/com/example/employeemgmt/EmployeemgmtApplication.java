2e7d91c535c8d5df7b7c3adb88fedcf317a48f6e
Sebastian T F <sebastian.tf97@gmail.com>
2021-01-08 13:47:41 +0530

    move ModelMapper Bean to EmployeemgmtApplication

--- src/main/java/com/example/employeemgmt/EmployeemgmtApplication.java
+++ src/main/java/com/example/employeemgmt/EmployeemgmtApplication.java
@@ -1,11 +1,19 @@
 package com.example.employeemgmt;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeemgmtApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
+	}
 
 	public EmployeemgmtApplication(@Value("${spring.profiles.active}") String activeProfile) {
 		System.out.println("Running on "+ activeProfile + " environment");
